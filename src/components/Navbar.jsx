9df4c2fd09dbfadb47ba83fcfe2ad93c26257766
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-09-17 22:53:38 +0400

    add drawer in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import "./navbar.css";
+import { useState } from "react";
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <div className="nav-layout">
@@ -7,7 +9,7 @@ function Navbar() {
           {/*logo*/}
           <a href="/" className="logo-style">
             <img src="/images/logo.png" alt="Logo" width={80} height={60} />
-            <span>MeraEstate</span>
+            <span className="logo-name">MeraEstate</span>
           </a>
           {/*nav items*/}
           <div className="nav-items">
@@ -17,14 +19,29 @@ function Navbar() {
             <a href="/">Agents</a>
           </div>
         </div>
-        <div className="right">
-          <a href="/login">
-            <button className="logIn">Login</button>
-          </a>
-          <a href="/signup">
-            <button className="signUp">Sign Up</button>
-          </a>
+        {!isOpen && (
+          <div className="right">
+            <a href="/login">
+              <button className="logIn">Login</button>
+            </a>
+            <a href="/signup">
+              <button className="signUp">Sign Up</button>
+            </a>
+          </div>
+        )}
+        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+          <img src="/images/menu.png" alt="Menu" width={25} height={25} />
         </div>
+        {isOpen && (
+          <div className="menu">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Agents</a>
+            <a href="/login">Login</a>
+            <a href="/signup">Sign Up</a>
+          </div>
+        )}
       </div>
     </div>
   );
