c44e96d43a44f0e657580d1e3fa9b776614aa0b8
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-09-23 12:51:16 +0400

    update search component

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,14 +1,35 @@
 import "./search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 function Search() {
+  const [isClicked, setClicked] = useState("btn-buy");
+
   return (
     <div className="search-box">
       <div className="type">
-        <button>buy</button>
-        <button>rent</button>
+        <button
+          className={isClicked === "btn-buy" ? "selected" : ""}
+          onClick={() => setClicked("btn-buy")}
+        >
+          Buy
+        </button>
+        <button
+          className={isClicked === "btn-rent" ? "selected" : ""}
+          onClick={() => setClicked("btn-rent")}
+        >
+          Rent
+        </button>
       </div>
       <form>
-        <input type="text" name="location" placeholder="city Location" />
         <input
+          className="location"
+          type="text"
+          name="location"
+          placeholder="city Location"
+        />
+        <input
+          className="min-price"
           type="number"
           name="minPrice"
           min={0}
@@ -16,13 +37,16 @@ function Search() {
           placeholder="Min Price "
         />
         <input
+          className="max-price"
           type="number"
           name="maxPrice"
           min={0}
           max={10000000}
           placeholder="Max Price "
         />
-        <i class="fa-solid fa-magnifying-glass"></i>
+        <button>
+          <FontAwesomeIcon className="icon-search" icon={faMagnifyingGlass} />
+        </button>
       </form>
     </div>
   );
